388ce90c9623470c2ddb41813436a42d82ea88fc
Andre Morryson Sousa Alves <112003817+4BurnerStove@users.noreply.github.com>
2023-03-14 14:07:02 -0300

    Add files via upload

---
+++ fav.js
@@ -0,0 +1,135 @@
+import { GitHubData } from './GitHubData.js'
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorite:'))
+  }
+
+  save() {
+    localStorage.setItem('@github-favorite:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if (userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GitHubData.search(username)
+
+      if (user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      entry => entry.login !== user.login
+    )
+
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('table .UsersTable')
+    this.tbodyEmpty = this.root.querySelector('#TableInitialBody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.gitSearch button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.gitSearch input')
+
+      this.add(value)
+    }
+  }
+
+  update() {
+    this.removeAllTr()
+    this.emptyPage()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = `/${user.login}`
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        this.delete(user)
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  emptyPage() {
+    const empyPage = document.querySelector('.trEmpty')
+
+    if (this.entries == null) {
+      empyPage.classList.remove('hide')
+    }
+    if (this.entries != null) {
+      empyPage.classList.add('hide')
+    }
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+      <td class="user">
+        <img src="" alt="">
+        <a href="" target="_blank">
+          <p></p>
+          <span></span>
+        </a>
+      </td>
+      <td class="repositories">
+        
+      </td>
+      <td class="followers">
+        
+      </td>
+      <td>
+        <button class="remove">Remover</button>
+      </td>
+    `
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
+      tr.remove()
+    })
+  }
+}
